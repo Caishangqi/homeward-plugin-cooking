024283536ce5d4aea102f7d94f286552684cec2a
Caishangqi <2470159730@qq.com>
2022-05-15 01:56:18 -0700

    Initial commit

---
+++ src/main/java/homeward/plugin/homewardcooking/Homewardcooking.java
@@ -0,0 +1,18 @@
+package homeward.plugin.homewardcooking;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Homewardcooking extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
