9715ef0ddd5210657f5c0016951309352155f3cd
Caishangqi <2470159730@qq.com>
2022-05-20 02:16:44 -0700

    Modify Main part of the Plugins

--- src/main/java/homeward/plugin/homewardcooking/Homewardcooking.java
+++ src/main/java/homeward/plugin/homewardcooking/Homewardcooking.java
@@ -4,15 +4,32 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Homewardcooking extends JavaPlugin {
 
+    //插件主类
+    public static Homewardcooking plugin;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        loadDependencies();
 
+
+    }
+
+    private void loadDependencies() {
+        plugin = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        unloadDependencies();
+        disableTask();
+    }
+
+    private void disableTask() {
+    }
+
+    private void unloadDependencies() {
     }
 
 }
