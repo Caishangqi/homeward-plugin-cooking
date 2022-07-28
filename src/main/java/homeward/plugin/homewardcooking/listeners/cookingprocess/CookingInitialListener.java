c27fe886543b31375e077088457c8849ae3ff7aa
Caishangqi <2470159730@qq.com>
2022-07-28 15:51:40 -0700

    输出物品兼容MMOITEMS 并且修复了堆叠BUG

--- src/main/java/homeward/plugin/homewardcooking/listeners/cookingprocess/CookingInitialListener.java
+++ src/main/java/homeward/plugin/homewardcooking/listeners/cookingprocess/CookingInitialListener.java
@@ -40,15 +40,19 @@ public class CookingInitialListener implements Listener {
             ItemStack objectMaterial = (ItemStack) recipe.getMainOutPut().getObjectMaterial();
 
 
+            //如果GUI输出直接是空的并且配方匹配，直接开始工作
             if (HomewardCooking.GUIPools.get(event.getLocationKey()).getInventory().getItem(CookingGUI.outputSlot) == null) {
                 //TODO 开始处理配方
                 evokeCookingProcessEvent(event, recipe);
+
+                //如果GUI不是空的但是预期输出+GUI输出槽的总和没有超过64，开始工作
             } else if (objectMaterial.isSimilar(HomewardCooking.GUIPools.get(event.getLocationKey()).getInventory().getItem(CookingGUI.outputSlot))
                     && HomewardCooking.GUIPools.get(event.getLocationKey()).getInventory().getItem(CookingGUI.outputSlot).getAmount() + objectMaterial.getAmount() <= 64) {
                 //TODO 开始处理配方
                 evokeCookingProcessEvent(event, recipe);
 
             } else {
+                //被占用了，直接报错
                 event.getPlayer().playSound(event.getPlayer(), Sound.ENTITY_VILLAGER_NO, 1.0F, 1.0F);
                 CommonUtils.getInstance().sendPluginMessageInServer(event.getPlayer(), "&c物品堆叠满层!");
             }
